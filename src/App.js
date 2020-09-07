33f15770b9026e134758389148e1ac3f98c42574
Max <maksimavramenko33@gmail.com>
2020-09-07 18:32:57 +0300

    did hw12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Contracts from './components/contracts.js';
 function App() {
   return (
-    <div className="App">
-      <h1>Max</h1>
-      <h1>Max</h1>
-      <h1>Max</h1>
-      <h1>Max</h1>
-      <h1>Max</h1>
-      <h1>Max</h1>
-    </div>
+    <Contracts />
   );
 }
-
 export default App;
